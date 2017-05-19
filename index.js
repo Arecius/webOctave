5ac079126196a035a334374fab36504aaf8fd578
Daniel Henriquez <dhenriquezn@gmail.com>
2017-05-19 11:57:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
 var express = require('express'); // call express
 var app = express(); // define our app using express
 var bodyParser = require('body-parser');
@@ -19,20 +15,10 @@ var upload = multer({
     }
 });
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-/*
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.text());
-*/
 var port = process.env.PORT || 8080; // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
 var router = express.Router(); // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 
 router.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -86,17 +72,8 @@ router.get( '/retrieve', ( req, res ) =>{
     res.sendFile(path.join(__dirname, req.query.id ));
 });
 
-    // more routes for our API will happen here
 
-    // REGreISTER OUR ROUTES -------------------------------
-    // all of our routes will be prefixed with /api
     app.use('/api', router);
 
-
-
-
-
-// START THE SERVER
-// =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
